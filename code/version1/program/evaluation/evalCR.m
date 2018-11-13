ecad16c79f1ba2e4d21ea6b89cf52380ade18662
Enrico <bergmaen@math.hu-berlin.de>
2018-11-13 16:24:39 +0100

    Add functionality to save results of evalCR to a textfile and add temporary output files to gitignore

--- code/version1/program/evaluation/evalCR.m
+++ code/version1/program/evaluation/evalCR.m
@@ -1,7 +1,9 @@
 function evalCR(path)
 
   if nargin < 1
-    path = '../../../../results/forPlot_h=1e-3/nonconforming/zeroInitial/';
+    hString = '1e-6';
+    path = sprintf('../../../../results/forPlot_h=%s/nonconforming/zeroInitial/',...
+      hString);
   end
 
   addpath(genpath('../'),genpath('../../../utils/'));
@@ -12,7 +14,7 @@ function evalCR(path)
 
   nDofVec = [];
   l2ErrorVec = [];
-  l2EnergyErrorVec = [];
+  % l2EnergyErrorVec = [];
   
   for j=1:nDirs
     myVars = {'u','c4n','n4e','alpha','delta','red'};
@@ -26,25 +28,30 @@ function evalCR(path)
     uExact = @(x)gUexact(x,alpha,delta);
     l2ErrorVec(end+1) = sqrt(sum(error4eCRL2(c4n,n4e,uExact,u)));
 
-    gradUexact = @(x)gradGuExact(x,delta);
-    l2EnergyErrorVec(end+1) = sqrt(sum(error4eCREnergy(c4n,n4e,gradUexact,u)));
+    % gradUexact = @(x)gradGuExact(x,delta);
+    % l2EnergyErrorVec(end+1) = sqrt(sum(error4eCREnergy(c4n,n4e,gradUexact,u)));
   end
 
   [nDofVec,I] = sort(nDofVec);
   l2ErrorVec = l2ErrorVec(I);
-  l2EnergyErrorVec = l2EnergyErrorVec(I);
+  % l2EnergyErrorVec = l2EnergyErrorVec(I);
+   
+  name = sprintf('l2Error_%s.txt',hString);
+  file = fopen(name,'w');
+  fprintf(file,'%s %s\n','nDof','l2Error');
+  fprintf(file,'%d %.8e\n',[nDofVec;l2ErrorVec]);
+  fclose(file);
 
-
-  % further plots  
-  errorL2Fig = figure('visible','on'); 
+  l2ErrorFig = figure('visible','off'); 
   loglog(nDofVec,l2ErrorVec);
   xlabel('nDof');
   ylabel('exact L2 error');
-  saveas(errorL2Fig,'l2Test1e-3.png');
+  name = sprintf('l2Error_%s.png',hString);
+  saveas(l2ErrorFig,name);
 
-  energyErrorFig = figure('visible','on'); 
-  loglog(nDofVec,l2EnergyErrorVec);
-  xlabel('nDof');
-  ylabel('exact energy error');
-  saveas(energyErrorFig,'energyTest1e-3.png');
+  % energyErrorFig = figure('visible','on'); 
+  % loglog(nDofVec,l2EnergyErrorVec);
+  % xlabel('nDof');
+  % ylabel('exact energy error');
+  % saveas(energyErrorFig,'energyTest1e-3.png');
  end
