1f197bee6b971a6fa8d0d268887a43f0c4211730
Enrico <bergmaen@math.hu-berlin.de>
2019-04-01 16:30:56 +0200

    Use Aitken extrapolation to approximate the limit for the iteration in question

--- code/version1/program/tvRegPrimalDual.m
+++ code/version1/program/tvRegPrimalDual.m
@@ -88,18 +88,43 @@ function  [u,corrVec,energyVec,nrDof] = ...
           firstScreenshot,datestr(now,'yy_mm_dd_HH_MM_SS'));
         mkdir(dirName);
 
-        name = sprintf('%s/energyVec.txt',dirName);
+        limitAitken = ...
+          (energyVec(end)*energyVec(end-2)-energyVec(end-1)^2)/...
+          (energyVec(end-2)-2*energyVec(end-1)+energyVec(end));
+
+        name = sprintf('%s/AitkenLimitEnergyVec.txt',dirName);
         file = fopen(name,'w');
+        fprintf(file, 'Aitken Limit\n%.8g\n',limitAitken);
         fprintf(file, '%.8g\n',energyVec);
         fclose(file);
 
+        % Use Aitken Extrapolation as limit
+        energyRateAitkenFig = figure('visible','off');
+        loglog(energyVec-limitAitken);
+        ftitle=sprintf('|E_{NC}(u_{j})-E_{NC}(u_{aitken})|');
+        title(ftitle);
+        xlabel('number of iterations');
+        ylabel('|E_{NC}(u_{j})-E_{NC}(u_{aitken})|');
+        fName = sprintf('%s/EnergyRateFigRed%dAitken.png',dirName,red);
+        saveas(energyRateAitkenFig,fName);
+
+        energyRateAitkenFigSemilog = figure('visible','off');
+        semilogy(energyVec-limitAitken);
+        ftitle=sprintf('|E_{NC}(u_{j})-E_{NC}(u_{aitken})|');
+        title(ftitle);
+        xlabel('number of iterations');
+        ylabel('|E_{NC}(u_{j})-E_{NC}(u_{aitken})|');
+        fName = sprintf('%s/EnergyRateFigSemilogRed%dAitken.png',dirName,red);
+        saveas(energyRateAitkenFigSemilog,fName);
+
+        % Use last energyVec value as limit
         energyRateFig = figure('visible','off');
         loglog(energyVec-energyVec(end));
         ftitle=sprintf('|E_{NC}(u_{j})-E_{NC}(u_{end})|');
         title(ftitle);
         xlabel('number of iterations');
         ylabel('|E_{NC}(u_{j})-E_{NC}(u_{end})|');
-        fName = sprintf('%s/energyRateFigRed%d.png',dirName,red);
+        fName = sprintf('%s/EnergyRateFigRed%d.png',dirName,red);
         saveas(energyRateFig,fName);
 
         energyRateFigSemilog = figure('visible','off');
@@ -108,7 +133,7 @@ function  [u,corrVec,energyVec,nrDof] = ...
         title(ftitle);
         xlabel('number of iterations');
         ylabel('|E_{NC}(u_{j})-E_{NC}(u_{end})|');
-        fName = sprintf('%s/energyRateFigSemilogRed%d.png',dirName,red);
+        fName = sprintf('%s/EnergyRateFigSemilogRed%d.png',dirName,red);
         saveas(energyRateFigSemilog,fName);
       end
 
