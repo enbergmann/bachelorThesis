69ce2b6bdb16d0eb2fb3dbace73c0859a1f08928
Enrico <bergmaen@math.hu-berlin.de>
2019-07-25 14:55:46 +0200

    Change structure of code to compute exact energy

--- code/version1/program/examples/exactEnergy.m
+++ code/version1/program/examples/exactEnergy.m
@@ -1,18 +1,46 @@
 function exactEnergy(red,degree,alpha,delta)
+  % compute all energies until red
   addpath(genpath('../'),genpath('../../../utils/'));
   f = @(x) g(x,alpha,delta);  
   u = @(x) gUexact(x,alpha,delta);
   gradU = @(x) gradGuExact(x,delta);
-  % [c4n,n4e] = computeGeometryPolygon(red);
-  %[c4n,n4e] = computeGeometryCriss(red);
-  [c4n,n4e] = computeGeometryBigSquare(red);
-  area4e = computeArea4e(c4n,n4e);
-  
-  energy = sum(...
-      integrate(@(n4p,Gpts4p,Gpts4ref)(...
-    alpha/2*u(Gpts4p).^2 + sqrt(sum(gradU(Gpts4p).^2,2)) - f(Gpts4p).*u(Gpts4p))...
-    ,c4n,n4e,degree+1,area4e))
-  
+
+  geometry = 'polygonLevel0';
+  [c4n,n4e,n4sDb,n4sNb] = computeGeometryPolygon(0);
+  % [c4n,n4e] = computeGeometryCriss(red);
+  % [c4n,n4e,n4sDb,n4sNb] = computeGeometryBigSquare(0);
+
+  dirName = sprintf(...
+    '../../../../results/exactEnergy/nonconforming/%s/%s',...
+    geometry,datestr(now,'yy_mm_dd_HH_MM_SS'));
+  mkdir(dirName);
+  name = sprintf('%s/exactEnergy.txt',dirName);
+  file = fopen(name,'w');
+  fprintf(file, 'nDoF   energy\n');
+
+  for level = 0:red
+    [c4n,n4e,n4sDb,n4sNb] = refineUniformRed(c4n,n4e,n4sDb,n4sNb);
+    if strcmp(geometry,'polygon')
+      temp=unique(n4sDb);
+      c4n(temp,:)=c4n(temp,:)./repmat(sqrt(c4n(temp,1).^2+c4n(temp,2).^2),1,2);
+    end
+    area4e = computeArea4e(c4n,n4e);
+    
+    energy = sum(...
+        integrate(@(n4p,Gpts4p,Gpts4ref)(...
+      alpha/2*u(Gpts4p).^2 + sqrt(sum(gradU(Gpts4p).^2,2)) - f(Gpts4p).*u(Gpts4p))...
+      ,c4n,n4e,degree+1,area4e))
+    
+
+    s4e = computeS4e(n4e);
+    nrSides = max(max(s4e));
+    dof = computeDof(n4e,nrSides,n4sDb,n4sNb);
+    nrDof = length(dof);
+
+    fprintf(file, '%d   %.30g\n',nrDof,energy);
+  end
+
+  fclose(file);
    
   % nrElems = size(n4e,1);
   % nrSides = max(max(s4e));
