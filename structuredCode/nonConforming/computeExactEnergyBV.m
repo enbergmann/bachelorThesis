5e5650f77bd11e2837cd6fcca7a4fb47d32ce93c
Enrico <bergmaen@math.hu-berlin.de>
2019-11-07 18:54:35 +0100

    Add precision as additional termination criterion to the iteration

--- structuredCode/nonConforming/computeExactEnergyBV.m
+++ structuredCode/nonConforming/computeExactEnergyBV.m
@@ -1,37 +1,38 @@
 function computeExactEnergyBV(geometry, fStr, fStrParams, uStr, uStrParams, ...
     gradUStr, gradUStrParams, parAlpha, parBeta, ...
-    minNrDof, minSignificantDigits, degree4Integrate)
+    minNrDof, minPrecision, degree4Integrate)
 
-% optional  minNrDof, significantDigits degree4Integratke,
-  % TODO write interface documentation
-  % compute all energies until at least minNrDof
 % Computes the degrees of freedom for the CR_0^1 FEM of the triangulation given
 % by [n4e, n4sDb, n4sNb].
 %
-% computeDofCR.m
-% input:  currData - struct with fields:
-%                        n4sDb: nodes for Dirichlet boundary sides
-%                          s4n: sides for nodes
-%                      nrSides: number of sides
+% computeExactEnergyBV.m
+% input:  geometry         - 'str'
+%         fStr             -
+%         fStrParams       -
+%         uStr             -
+%         uStrParams       -
+%         gradUSr          -
+%         gradUStrParams   -
+%         parAlpha         -
+%         parBeta          - 
+%         minNrDof         - 
+%         minPrecision     -
+%         degree4Integrate -
 %
-% output: dof      - '(1 x nrDof)-dimensional double array' where the j-th row 
-%                    contains the number of the j-th degree of freedom
+% output: -
+%         
 
-% TODO mode parameter? either compute until at least minNrDof Dofs or
-% until at least significantDigits significant digits (change from one mesh 
-% to next smaller than 1e-'significant digits' or sth) or both
+  addpath(genpath(pwd), genpath('../utils/'));
 
   if nargin < 12
     degree4Integrate = 20;
+    if nargin < 11
+      minPrecision = 3;
+      if nargin < 10
+        minNrDof = 1e4;
+      end
+    end
   end
-  if nargin < 11
-    minSignificantDigits = 0;
-  end
-  if nargin < 10
-    minNrDof = 1e4;
-  end
-
-  addpath(genpath(pwd), genpath('../utils/'));
 
   f = @(x) feval(fStr, x, fStrParams); 
   u = @(x) feval(uStr, x, uStrParams); 
@@ -56,7 +57,7 @@ function computeExactEnergyBV(geometry, fStr, fStrParams, uStr, uStrParams, ...
   warning('on', 'MATLAB:MKDIR:DirectoryExists');
 
   nrDofVec = [];
-  energyVec = [];
+  energyVec = [0];
 
   while true
     [c4n, n4e, n4sDb, n4sNb] = refineUniformRed(c4n, n4e, n4sDb, n4sNb);
@@ -66,7 +67,7 @@ function computeExactEnergyBV(geometry, fStr, fStrParams, uStr, uStrParams, ...
         c4n(temp, :)./repmat(sqrt(c4n(temp, 1).^2 + c4n(temp, 2).^2), 1, 2);
     end
 
-    area4e = computeArea4e(c4n, n4e);
+    area4e  = computeArea4e(c4n, n4e);
     
     energyVec(end+1) = sum(...
         integrate(@(n4p, Gpts4p, Gpts4ref)(...
@@ -74,30 +75,27 @@ function computeExactEnergyBV(geometry, fStr, fStrParams, uStr, uStrParams, ...
       - f(Gpts4p).*u(Gpts4p)), ...
       c4n, n4e, degree4Integrate+1, area4e));
     
-    %TODO compute significant digits 
-
     s4e = computeS4e(n4e);
     n4s = computeN4s(n4e);
-    tempStruct = struct;
     tempStruct.n4sDb = n4sDb;
     tempStruct.s4n = computeS4n(n4e, n4s);
     tempStruct.nrSides = max(max(s4e));
     dof = computeDofCR(tempStruct);
     nrDofVec(end+1) = length(dof);
-    if nrDofVec(end) > minNrDof
-      %TODO also compare to minSignDigits and just terminate, if both are
-      %satisfied
+    if nrDofVec(end) > minNrDof ...
+        && abs(energyVec(end)-energyVec(end-1)) < 10^(-minPrecision)
       break
     end
+
+    fprintf('---------------------------------------\nenergy  nrDof\n');
+    fprintf('%.20g  %d\n', energyVec(end), nrDofVec(end));
   end
 
-  name = sprintf('%s/%d_%d.txt', dirName, minSignificantDigits, nrDofVec(end));
-  %TODO use actual number of significant digits (will have those in the end
-  %after checking them every time)
+  name = sprintf('%s/minPrecision_%d_nrDof_%d.txt', dirName, minPrecision, nrDofVec(end));
 
   file = fopen(name, 'w');
   fprintf(file, 'nrDof   energy\n');
-  fprintf(file, '%d   %.30g\n', [nrDofVec; energyVec]);
+  fprintf(file, '%d   %.30g\n', [nrDofVec; energyVec(2:end)]);
   fclose(file);
    
   % nrElems = size(n4e,1);
@@ -110,5 +108,4 @@ function computeExactEnergyBV(geometry, fStr, fStrParams, uStr, uStrParams, ...
   % [~,MAMANC] = computeFeMatrices(c4n,n4e,s4e,area4e,nrElems);
 
   % du = computeGradientNC(c4n,n4e,u);
-  % E = computeEnergy(area4e,u,du,alpha,temp,MAMANC);
-end
+  % E = computeEnergy(area4e,u,du,alpha,temp,MAMANC); end
