3edf3e72dc1d47a762bcae40a593cfc1815d3bea
Enrico <bergmaen@math.hu-berlin.de>
2021-06-20 12:32:31 +0200

    Note down all important estimates for questions to Tien

--- structuredCode/nonconforming/startAlgorithmCR.m
+++ structuredCode/nonconforming/startAlgorithmCR.m
@@ -166,15 +166,6 @@ function startAlgorithmCR(benchmark)
       computeNormDiffRhsSolCrSquared4e(params, currData, output);
 
     % compute guaranteed lower energy bound
-    % TODO here the guaranteed upper bound, whatever that means, can be
-    %      included after it has been implemented in computeGueb
-    %   for that enriching operator code has to be extracted from
-    %   prolongationJ1 to a own function computeJ1 and I also need
-    %   a function that computes a CR function from a Courant function 
-    %   (should be possible, we also can extract that from
-    %   prolongationJ1)
-    %   after that we can also simplify prolongationJ1 since we externalize
-    %   a lot of code
     if rhsGradientKnown
       glebCurr = computeGleb(params, currData, output);
       outputLvlEnergy.gleb(end+1, 1) = glebCurr;
@@ -182,6 +173,12 @@ function startAlgorithmCR(benchmark)
       if useExactEnergy
         outputLvlEnergy.diffGlebExactE(end+1, 1) = exactEnergy - glebCurr;
       end
+      % TODO here the guaranteed upper bound, whatever that means, can be
+      %      included after it has been implemented in computeGueb
+      % definitely save more thinks that might prove usefull
+      %    including:  E_NC(J1 uCR)
+      %                gueb
+      %                ||J1 ucr - ucr||
     end
 
     % ESTIMATE
