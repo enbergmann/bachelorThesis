f722217d9830899351f683a451646f8f98dfcb9c
Enrico <bergmaen@math.hu-berlin.de>
2021-07-03 16:56:37 +0200

    Add circle benchmarks

--- structuredCode/utils/functions/middleCircle.m
+++ structuredCode/utils/functions/middleCircle.m
@@ -3,6 +3,5 @@ function y = middleCircle(x, args)
   r = sqrt(sum(x.^2, 2));
   y = zeros(length(r), 1);
 
-  % TODO
-  y(-1/2<x(:, 1) & x(:, 1) < 1/2 & -1/2< x(:, 2) & x(:, 2)<1/2) = parAlpha;
+  y(r<=1/2) = parAlpha;
 end
